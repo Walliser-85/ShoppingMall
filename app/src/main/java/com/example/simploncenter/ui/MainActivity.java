7a1483331f065b9029876b3a804f85d9c54c3d81
solst2 <47793395+solst2@users.noreply.github.com>
2019-04-24 09:11:23 +0200

    ...

--- app/src/main/java/com/example/simploncenter/ui/MainActivity.java
+++ app/src/main/java/com/example/simploncenter/ui/MainActivity.java
@@ -2,20 +2,9 @@ package com.example.simploncenter.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.simploncenter.R;
-import com.example.simploncenter.db.repository.ShopRepository;
 import com.example.simploncenter.ui.article.Articles;
 import com.example.simploncenter.ui.shop.Shops;
 
@@ -31,10 +20,7 @@ public class MainActivity extends BaseActivity{
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return;
-        }
+        moveTaskToBack(true);
     }
 
     @Override
